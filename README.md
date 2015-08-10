3abc6f91d0c8ac02ca2118e30d0f77dd57193484
Evan Turner <evturn@gmail.com>
2015-08-10 01:20:12 -0400

    creating root directoy with user generated name and complete project directory structure

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # generator-faraday [![Build Status](https://secure.travis-ci.org/evturn/generator-faraday.png?branch=master)](https://travis-ci.org/evturn/generator-faraday)
 
+### Generator for the 20th Century
+
 > [Yeoman](http://yeoman.io) generator
 
 ```bash
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,57 +1,118 @@
 'use strict';
-var yeoman = require('yeoman-generator');
+var generators = require('yeoman-generator');
 var chalk = require('chalk');
-var yosay = require('yosay');
+var _ = require('lodash');
+var mkdirp = require('mkdirp');
 
-module.exports = yeoman.generators.Base.extend({
+module.exports = generators.Base.extend({
+
+  constructor: function() {
+    generators.Base.apply(this, arguments);
+
+  },
+
   prompting: function () {
     var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the tremendous ' + chalk.red('Faraday') + ' generator!'
-    ));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
-
+    this.log(
+      chalk.red('Faraday') + ', generator for the 20th Century.'
+    );
+    this.prompt({
+      type    : 'input',
+      name    : 'name',
+      message : 'Project name',
+      default : this.appname
+    }, function (props) {
+      this.log(props.name + ' it is.');
+      this.projectName = props.name;
       done();
     }.bind(this));
   },
 
-  writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
-    },
-
-    projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
-    }
+  makeDirectories: function() {
+    mkdirp.sync(this.projectName, function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/dist', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/dist/js', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/dist/css', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/dist/fonts', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/dist/img', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/views', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/views/layouts', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/views/partials', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/config', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/routes', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/assets', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/assets/fonts', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/assets/img', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/assets/less', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/assets/less/base', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/assets/less/vendor', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/assets/less/partials', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/assets/js', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
+    mkdirp.sync(this.projectName + '/assets/js/vendor', function (err, made) {
+      if (err) console.error(err)
+      else console.log('Created ' + made);
+    });
   },
 
-  install: function () {
-    this.installDependencies();
+  _onError: function () {
+    this.log('I think I ate something funny.');
   }
 });
