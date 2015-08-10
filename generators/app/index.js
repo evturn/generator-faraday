3ec37d434b01abf59bb1e0357f59de9ad9d555a1
Evan Turner <evturn@gmail.com>
2015-08-09 22:19:56 -0400

    running yo command runs generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,11 +3,21 @@ var generators = require('yeoman-generator');
 module.exports = generators.Base.extend({
 
   constructor: function() {
+
     generators.Base.apply(this, arguments);
 
     this.option('less');
     this.option('gulp');
     this.option('handlebars');
     this.option('babel');
+  },
+
+  method1: function() {
+    console.log('method 1');
+  },
+
+  method2: function() {
+    console.log('method 2');
   }
+
 });
