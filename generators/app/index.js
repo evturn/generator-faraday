8b86e1732a968f8fe5b7292385b3716979104bb2
Evan Turner <evturn@gmail.com>
2015-08-09 22:44:06 -0400

    created options hash for less support

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,4 +1,5 @@
 var generators = require('yeoman-generator');
+var _ = require('lodash');
 
 module.exports = generators.Base.extend({
 
@@ -6,10 +7,16 @@ module.exports = generators.Base.extend({
 
     generators.Base.apply(this, arguments);
 
+    this.argument('appname', {
+      type     : String,
+      required : true
+
+    });
+    this.appname = _.camelCase(this.appname);
+
+
     this.option('less');
-    this.option('gulp');
-    this.option('handlebars');
-    this.option('babel');
+    this.scriptSuffix = (this.options.less ? ".less" : ".css");
   },
 
   method1: function() {
